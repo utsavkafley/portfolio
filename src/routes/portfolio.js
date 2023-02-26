d3db23d530309f4c490adb92c8408714cc45046d
Utsav Kafley <vastukafley@gmail.com>
2023-02-26 12:06:01 -0500

    add github, project link

--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -5,18 +5,24 @@ export default function Portfolio() {
       description:
         "A simple app utilizing the National Parks API that allows users to search for national parks, filter by states, and view details about each park.",
       stack: ["react", "html", "css", "javascript", "tailwindcss"],
+      github: "https://github.com/utsavkafley/npfinder",
+      link: "https://npfinder.vercel.app/",
     },
     {
       name: "Penn Tool Library",
       description:
         "A online store built to support the idea of a community tool lending library. The site allows users to browse tools, add them to their cart, and send borrow requests.",
       stack: ["python", "html", "css", "flask", "mysql"],
+      github: "https://github.com/utsavkafley/",
+      link: "",
     },
     {
       name: "Bookish",
       description:
         "A simple app that allows users to manage their library, search for books, and add books to their shelves. Connects on the backend with API built using node.js and express. Mongodb database.",
       stack: ["react", "html", "css", "javascript", "mongodb", "nodejs"],
+      github: "https://github.com/utsavkafley/",
+      link: "",
     },
   ];
 
@@ -32,7 +38,7 @@ export default function Portfolio() {
                 <h1 className="text-2xl font-bold">{project.name}</h1>
                 <div className="flex flex-row space-x-1">
                   <a
-                    href=""
+                    href={project.github}
                     className="px-2 py-1  rounded border-2 border-secondary hover:bg-secondary"
                   >
                     <img
@@ -42,7 +48,7 @@ export default function Portfolio() {
                     />
                   </a>
                   <a
-                    href=""
+                    href={project.link}
                     className="px-2 py-1 rounded border-2 border-secondary hover:bg-secondary"
                   >
                     <img
